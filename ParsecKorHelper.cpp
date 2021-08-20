634f4bccad567c23343eb19098a6a52e3f62658a
bspfp <kwanghochoi@hotmail.com>
2021-08-20 10:40:22 +0900

    버그 수정

--- ParsecKorHelper.cpp
+++ ParsecKorHelper.cpp
@@ -18,43 +18,48 @@ constexpr UINT HOTKEY_HANJA_MOD = MOD_CONTROL | MOD_SHIFT;
 constexpr UINT HOTKEY_HANJA_VK = VK_RETURN;
 
 INT_PTR CALLBACK AppDlgProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
-	auto regHotkey = [&](int id, UINT mod, UINT vk, const wchar_t* desc) {
-		if (!RegisterHotKey(hwnd, id, mod, vk)) {
-			wstringstream ss;
-			ss << desc << L" 단축키 등록 실패: ";
-			auto err = GetLastError();
-			if (err == 1409)
-				ss << desc << L" 이미 사용 중인 키조합";
-			else
-				ss << desc << L" Error: " << err;
-			return false;
-		}
-		return true;
-	};
+	UNREFERENCED_PARAMETER(lparam);
 
 	switch (msg) {
 	case WM_INITDIALOG:
+	{
 		SendMessage(hwnd, WM_SETICON, ICON_SMALL, LPARAM(AppIcon));
 		SetWindowText(hwnd, APP_TITLE);
 
 		// 핫키 설정
+		auto regHotkey = [&](int id, UINT mod, UINT vk, const wchar_t* desc) {
+			if (!RegisterHotKey(hwnd, id, mod, vk)) {
+				wstringstream ss;
+				ss << desc << L" 단축키 등록 실패: ";
+				auto err = GetLastError();
+				if (err == 1409)
+					ss << desc << L" 이미 사용 중인 키조합";
+				else
+					ss << desc << L" Error: " << err;
+				return false;
+			}
+			return true;
+		};
+
 		if (!regHotkey(HOTKEY_ID_HANGUL, HOTKEY_HANGUL_MOD, HOTKEY_HANGUL_VK, L"한글") || !regHotkey(HOTKEY_ID_HANJA, HOTKEY_HANJA_MOD, HOTKEY_HANJA_VK, L"한자")) {
 			EndDialog(hwnd, 1);
 			return FALSE;
 		}
+
 		return TRUE;
+	}
 
 	case WM_HOTKEY:
 		switch (wparam) {
 		case HOTKEY_ID_HANGUL:
 			keybd_event(VK_HANGUL, 0xF2, KEYEVENTF_KEYUP, 0);
-			break;
+			return 0;
 
 		case HOTKEY_ID_HANJA:
 			keybd_event(VK_HANJA, 0xF1, KEYEVENTF_KEYUP, 0);
-			break;
+			return 0;
 		}
-		return 0;
+		break;
 
 	case WM_CLOSE:
 		EndDialog(hwnd, 0);
@@ -72,7 +77,7 @@ INT_PTR CALLBACK AppDlgProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
 		return 0;
 	}
 
-	return DefWindowProc(hwnd, msg, wparam, lparam);
+	return FALSE;
 }
 
 int WINAPI wWinMain(_In_ HINSTANCE hinst, _In_opt_ HINSTANCE, _In_ LPWSTR, _In_ int) {
